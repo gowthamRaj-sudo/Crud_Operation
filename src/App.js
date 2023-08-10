03ca0f51de98e8144ad33b51699049aa46e1498d
gowthamRaj-sudo <gowthamraj87544@gmail.com>
2023-08-11 02:46:31 +0530

    resove

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import Create from "./Create";
-import { ToastContainer } from "react-toastify";
-import View from "./View";
-import Edit from "./Edit";
-import ConfirmationModal from "./Component/ConfirmationModal";
 
 function App() {
   return (
